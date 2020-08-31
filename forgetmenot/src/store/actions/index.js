a78298dd11265023086a380c41865ce6d3e50146
Leila <nb.leila10@gmail.com>
2020-08-31 17:48:44 -0500

    Remove Redux store folder

--- forgetmenot/src/store/actions/index.js
+++
@@ -1,5 +0,0 @@
-export { addUser, logUser, getCurrentUser, updateUser } from "./userActions";
-
-export { fetchMessages, addMessage, updateMessage, deleteMessage, saveCurrentMessage } from "./messagesActions";
-
-export { getContacts, addContact } from "./contactsActions";
